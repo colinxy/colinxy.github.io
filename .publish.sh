6fa380c37445b4a273868f54f6149b7e3e743d9c
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-05-10 18:32:08 -0700

    improve publisher

--- .publish.sh
+++ .publish.sh
@@ -3,4 +3,11 @@
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "currently on '$branch' branch"
 
+# check branh
 [ "$branch" = 'master' ] || echo 'only publish on master branch' && exit 1
+
+# mv files from _site to root level
+SITE=_site
+echo "moving files to $SITE"
+git checkout source -- "$SITE"
+find "$SITE" -maxdepth 1 ! -path "$SITE" -print0 -exec mv -t "$SITE" {} +
