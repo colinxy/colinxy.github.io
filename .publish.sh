ca853de3f0c170ba00b4dc439f826d18465b2656
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-05-10 19:28:19 -0700

    just to make sure

--- .publish.sh
+++ .publish.sh
@@ -2,17 +2,21 @@
 
 set -e
 
+CURR_PATH=$(pwd)
+echo "$CURR_PATH"
+
 git add . && git commit -m 'just to make sure'
 
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "currently on '$branch' branch"
 
 # check branh
-[ "$branch" = 'master' ] || { echo 'only publish on master branch' && exit 1; }
+[ "$branch" = 'master' ] || { echo 'ONLY PUBLISH ON MASTER BRANCH' && exit 1; }
 
 
 # cleanup, keep only $KEEP in master branch
 KEEP=".git .gitignore .nojekyll README.md .publish.sh"
+echo "clear site, keep only $KEEP"
 find . -maxdepth 1 ! -name '.' ! -name '.git' ! -name '.gitignore' ! -name '.nojekyll' ! -name 'README.md' ! -name '.publish.sh'  -exec rm -r {} \;
 
 # mv files from _site to root level
@@ -20,5 +24,5 @@ SITE=_site
 echo "moving files to $SITE"
 git checkout source -- "$SITE"
 
-
-# find "$SITE" -maxdepth 1 ! -path "$SITE" -exec gmv -t . {} +
+# generate site by moving lifting everything in $SITE to root level
+find "$SITE" -maxdepth 1 ! -path "$SITE" -exec gmv -t . {} +
