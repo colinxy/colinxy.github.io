6c61aa1e8a97a319d6460dfb21182f8c912fd507
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-05-10 19:35:58 -0700

    just to make sure

--- .publish.sh
+++ .publish.sh
@@ -2,8 +2,13 @@
 
 set -e
 
-CURR_PATH=$(pwd)
-echo "$CURR_PATH"
+BLOG='colinxy.github.io'
+
+# primary check, directory
+CURR_PATH=${PWD##*/}
+[ "$BLOG" = "$CURR_PATH" ] ||
+    { echo "NOT THE BLOG DIRECTORY" && exit 1; }
+
 
 git add . && git commit -m 'just to make sure'
 
@@ -11,7 +16,8 @@ branch=$(git rev-parse --abbrev-ref HEAD)
 echo "currently on '$branch' branch"
 
 # check branh
-[ "$branch" = 'master' ] || { echo 'ONLY PUBLISH ON MASTER BRANCH' && exit 1; }
+[ "$branch" = 'master' ] ||
+    { echo 'ONLY PUBLISH ON MASTER BRANCH' && exit 1; }
 
 
 # cleanup, keep only $KEEP in master branch
@@ -19,10 +25,15 @@ KEEP=".git .gitignore .nojekyll README.md .publish.sh"
 echo "clear site, keep only $KEEP"
 find . -maxdepth 1 ! -name '.' ! -name '.git' ! -name '.gitignore' ! -name '.nojekyll' ! -name 'README.md' ! -name '.publish.sh'  -exec rm -r {} \;
 
-# mv files from _site to root level
+# checkout site from source branch
 SITE=_site
 echo "moving files to $SITE"
 git checkout source -- "$SITE"
 
 # generate site by moving lifting everything in $SITE to root level
 find "$SITE" -maxdepth 1 ! -path "$SITE" -exec gmv -t . {} +
+# bash mv sucks, use GNU ls
+
+
+echo
+echo "=== you should commit and push now ==="
