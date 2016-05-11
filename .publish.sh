ec1d934afa22b67c1f0b98fe99c7565fd2df9f00
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-05-10 19:13:55 -0700

    just to make sure

--- .publish.sh
+++ .publish.sh
@@ -1,6 +1,24 @@
 #! /bin/sh
 
+set -e
+
+git add . && git commit -m 'just to make sure'
+
 branch=$(git rev-parse --abbrev-ref HEAD)
 echo "currently on '$branch' branch"
 
-[ "$branch" = 'master' ] || echo 'only publish on master branch' && exit 1
+# check branh
+[ "$branch" = 'master' ] || { echo 'only publish on master branch' && exit 1; }
+
+
+# cleanup, keep only $KEEP in master branch
+KEEP=".git .gitignore .nojekyll README.md .publish.sh"
+find . -maxdepth 1 ! -name '.git' ! -name '.gitignore' ! -name '.nojekyll' ! -name 'README.md' ! -name '.publish.sh'  -print
+
+# mv files from _site to root level
+SITE=_site
+echo "moving files to $SITE"
+git checkout source -- "$SITE"
+
+
+find "$SITE" -maxdepth 1 ! -path "$SITE" -exec gmv -t . {} +
--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 [My Blog](http://colinxy.github.io)
-
-This is the master branch. all content of _site/ and .jekyll
-will be present in this directory.
