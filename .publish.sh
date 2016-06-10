ee632445bc5808e7f350728b42590da5ca081041
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-06-10 04:36:42 -0700

    add color to publish msg

--- .publish.sh
+++ .publish.sh
@@ -10,30 +10,35 @@ CURR_PATH=${PWD##*/}
     { echo "NOT THE BLOG DIRECTORY" && exit 1; }
 
 
-git add . && git commit -m 'just to make sure'
+git add . && git commit -m 'blog update'
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-echo "==> currently on '$branch' branch"
+printf '\033[0;36m==>\033[0m '
+echo "currently on '$branch' branch"
 
-# check branh
+# check branch
 [ "$branch" = 'master' ] ||
     { echo 'ONLY PUBLISH ON MASTER BRANCH' && exit 1; }
 
 
 # cleanup, keep only $KEEP in master branch
 KEEP=".git .gitignore .nojekyll README.md .publish.sh"
-echo "==> clear site, keep only $KEEP"
+
+printf '\033[0;36m==>\033[0m '
+echo "clear site, keep only $KEEP"
 find . -maxdepth 1 ! -name '.' ! -name '.git' ! -name '.gitignore' ! -name '.nojekyll' ! -name 'README.md' ! -name '.publish.sh'  -exec rm -r {} \;
 
 # checkout site from source branch
 SITE=_site
-echo "==> moving files to $SITE"
-git checkout source -- "$SITE"
 
-# generate site by moving lifting everything in $SITE to root level
-find "$SITE" -maxdepth 1 ! -path "$SITE" -exec gmv -t . {} +
-# bash mv sucks, use GNU ls
+printf '\033[0;36m==>\033[0m '
+echo "moving files to $SITE"
+git checkout source -- "$SITE"  # blog content on source branch
+
+# generate site by moving lifting everything in $SITE to blog root level
+find "$SITE" -maxdepth 1 ! -path "$SITE" -exec mv {} . \;
 
 
 echo
-echo "=== you should commit and push now ==="
+echo "publish success"
+printf "\033[0;36m===\033[0m you should commit and push now \033[0;36m===\033[0m\n"
