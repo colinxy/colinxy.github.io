8e242bf3f46b54702aee35cf65b309da2d89f2db
Xinyu Yang <colin.yangxinyu@outlook.com>
2016-07-08 19:50:19 -0700

    improve publish flow

--- .publish.sh
+++ .publish.sh
@@ -13,7 +13,7 @@ CURR_PATH=${PWD##*/}
 # git add . && git commit -m 'blog update'
 
 branch=$(git rev-parse --abbrev-ref HEAD)
-printf '\033[0;36m==>\033[0m '
+printf '\033[0;36m==>\033[0;00m '
 echo "currently on '$branch' branch"
 
 # check branch
@@ -24,14 +24,14 @@ echo "currently on '$branch' branch"
 # cleanup, keep only $KEEP in master branch
 KEEP=".git .gitignore .nojekyll README.md .publish.sh"
 
-printf '\033[0;36m==>\033[0m '
+printf '\033[0;36m==>\033[0;00m '
 echo "clear site, keep only $KEEP"
 find . -maxdepth 1 ! -name '.' ! -name '.git' ! -name '.gitignore' ! -name '.nojekyll' ! -name 'README.md' ! -name '.publish.sh'  -exec rm -r {} \;
 
 # checkout site from source branch
 SITE=_site
 
-printf '\033[0;36m==>\033[0m '
+printf '\033[0;36m==>\033[0;00m '
 echo "moving files to $SITE"
 git checkout source -- "$SITE"  # blog content on source branch
 
@@ -41,4 +41,4 @@ find "$SITE" -maxdepth 1 ! -path "$SITE" -exec mv {} . \;
 
 echo
 echo "publish success"
-printf "\033[0;36m===\033[0m you should commit and push now \033[0;36m===\033[0m\n"
+printf "\033[0;36m===\033[0;00m you should commit and push now \033[0;36m===\033[0;00m\n"
